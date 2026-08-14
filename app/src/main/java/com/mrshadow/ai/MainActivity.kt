cfadd0315fc99dd31d35ea357665fa0b42b3ac4f
diasrispermana1-cell <diasrispermana1@gmail.com>
2026-08-15 01:03:36 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/mrshadow/ai/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.mrshadow.ai
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    private val websiteUrl =
+        "https://mr-shadow-ai.vercel.app/"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+
+        setContentView(webView)
+
+        webView.settings.apply {
+
+            javaScriptEnabled = true
+
+            domStorageEnabled = true
+
+            databaseEnabled = true
+
+            allowFileAccess = true
+
+            allowContentAccess = true
+
+            mediaPlaybackRequiresUserGesture = false
+
+            builtInZoomControls = false
+
+            displayZoomControls = false
+
+            loadWithOverviewMode = true
+
+            useWideViewPort = true
+        }
+
+        webView.webViewClient =
+            object : WebViewClient() {
+
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+
+                    return false
+                }
+            }
+
+        webView.webChromeClient =
+            WebChromeClient()
+
+        if (savedInstanceState == null) {
+
+            webView.loadUrl(websiteUrl)
+
+        } else {
+
+            webView.restoreState(
+                savedInstanceState
+            )
+        }
+
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+
+                override fun handleOnBackPressed() {
+
+                    if (webView.canGoBack()) {
+
+                        webView.goBack()
+
+                    } else {
+
+                        finish()
+                    }
+                }
+            }
+        )
+    }
+
+    override fun onSaveInstanceState(
+        outState: Bundle
+    ) {
+
+        webView.saveState(outState)
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+
+        webView.apply {
+
+            stopLoading()
+
+            clearHistory()
+
+            removeAllViews()
+
+            destroy()
+        }
+
+        super.onDestroy()
+    }
+}
