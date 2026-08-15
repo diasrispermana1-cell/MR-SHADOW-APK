d5a1db76e81ce0dcb87b72fcd547025bcde66d79
diasrispermana1-cell <diasrispermana1@gmail.com>
2026-08-15 14:44:36 +0700

    Update MainActivity.kt

--- app/src/main/java/com/mrshadow/ai/MainActivity.kt
+++ app/src/main/java/com/mrshadow/ai/MainActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -22,17 +20,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Buat WebView
         webView = WebView(this)
-
-        // Background hitam agar tidak terlihat putih saat loading
         webView.setBackgroundColor(Color.rgb(11, 11, 15))
 
         setContentView(webView)
 
-        // Konfigurasi WebView
         webView.settings.apply {
-
             javaScriptEnabled = true
             domStorageEnabled = true
             databaseEnabled = true
@@ -41,10 +34,9 @@ class MainActivity : AppCompatActivity() {
             allowContentAccess = true
 
             loadsImagesAutomatically = true
-            blockNetworkImage = false
-            blockNetworkLoads = false
 
-            mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+            mixedContentMode =
+                WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
 
             mediaPlaybackRequiresUserGesture = false
 
@@ -62,50 +54,29 @@ class MainActivity : AppCompatActivity() {
             cacheMode = WebSettings.LOAD_DEFAULT
         }
 
-        // WebView client
         webView.webViewClient = object : WebViewClient() {
 
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
-                request: WebResourceRequest?
+                request: android.webkit.WebResourceRequest?
             ): Boolean {
                 return false
-            }
-
-            @Deprecated("Deprecated in API 24")
-            override fun shouldOverrideUrlLoading(
-                view: WebView?,
-                url: String?
-            ): Boolean {
-                return false
-            }
-
-            override fun onReceivedError(
-                view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?
-            ) {
-                super.onReceivedError(view, request, error)
             }
         }
 
-        // Dukungan JavaScript dialog / fullscreen
         webView.webChromeClient = WebChromeClient()
 
-        // Load website
         if (savedInstanceState == null) {
             webView.loadUrl(websiteUrl)
         } else {
             webView.restoreState(savedInstanceState)
         }
 
-        // Tombol Back Android
         onBackPressedDispatcher.addCallback(
             this,
             object : OnBackPressedCallback(true) {
 
                 override fun handleOnBackPressed() {
-
                     if (webView.canGoBack()) {
                         webView.goBack()
                     } else {
@@ -124,12 +95,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
 
         if (::webView.isInitialized) {
-
             webView.stopLoading()
-
-            webView.webChromeClient = null
-            webView.webViewClient = null
-
             webView.clearHistory()
             webView.removeAllViews()
             webView.destroy()
